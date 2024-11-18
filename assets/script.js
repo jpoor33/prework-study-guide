e4e3fe706b26699e2826ab1673b1aaf7f98cb683
jpoor <jonathan.e.poor@gmail.com>
2024-11-18 12:02:58 -0500

    added an array for topics and a for loop in js

---
+++ assets/script.js
@@ -0,0 +1,23 @@
+const topic = ['HTML','CSS','Git','JavaScript']
+for (let i = 0; i <topic.length; i++){
+    console.log(topic[i]);
+}
+
+const shapes = ["triangle", "square", "pentagon", "circle"];
+for(let i = 0; i < shapes.length; i++) {
+    console.log(shapes[i]);
+}
+
+/*
+if (topic === 'HTML') {
+  console.log("Let's study HTML!");
+} else if (topic === 'CSS') {
+  console.log("Let's study CSS!");
+} else if (topic === 'Git') {
+  console.log("Let's study Git!");
+} else if (topic === 'JavaScript') {
+  console.log("Let's study JavaScript!");
+} else {
+  console.log('Please try again!');
+}
+*/
